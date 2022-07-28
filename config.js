a46bddd60ad12a2d720b31e6da5a4e9bf87b3f63
EnigmaticJihad <enigmatic.jihad@gmail.com>
2022-07-28 06:26:20 +0600

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ const CONFIG = {
 
 	// Weather
 	weatherKey: '96b75fc6ede2c4c112a812e3a7f360b2', // Write here your API Key
-	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
+	weatherIcons: 'Dark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
 
